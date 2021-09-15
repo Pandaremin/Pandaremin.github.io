661ddaa1da84b85ff73a30d3c9c1347545424855
Pandaremin <71176743+Pandaremin@users.noreply.github.com>
2021-09-15 20:34:31 +0630

    Update nocodejson.js

--- nocodejson.js
+++ nocodejson.js
@@ -1,4 +1,14 @@
+{"success":true,"rate":[
 {
-  "firstName": firstName,
-  "lastName": lastName
-}
+  "country": USA,
+  "rate": 1.00},
+{
+  "country": AED,
+  "rate": 5.00},
+{
+  "country": ARS,
+  "rate": 8.00},
+{
+  "country": AUD,
+  "rate": 3.00}
+]}
